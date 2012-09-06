d75d581fd90c4e9db69f6e15d9d48f4129ac9d99
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-09-06 19:03:55 +0200

    better reference restore

--- Products/csvreplicata/handlers/reference.py
+++ Products/csvreplicata/handlers/reference.py
@@ -9,12 +9,65 @@
 __author__ = """Eric BREHAULT <eric.brehault@makina-corpus.org>"""
 __docformat__ = 'plaintext'
 
-from Products.csvreplicata.exceptions import *
+from Products.csvreplicata import exceptions as exs
+from Acquisition import aq_base
 from base import CSVdefault
+import os
 
 import logging
 logger = logging.getLogger('HANDLER')
 
+from Products.CMFCore.utils import getToolByName
+
+def get_path(context, path):
+    p = getToolByName(context, 'portal_url')
+    portal = p.getPortalObject()
+    ppath = '/'.join(portal.getPhysicalPath())
+    cpath = '/'.join(context.getPhysicalPath())
+    qpath = path
+    if not qpath.startswith(ppath):
+        if qpath.startswith('/'):
+            c = ppath
+            qpath = qpath.strip('/')
+        else:
+            c = cpath
+        qpath = os.path.join(c, qpath)
+    target = None
+    catalog = getToolByName(context, 'portal_catalog')
+    res = catalog.search({'path':{'query': qpath,'depth':0}})
+    if len(res):
+        target = res[0].getObject()
+    #try restrictedtraverse
+    if not target:
+        try:
+            target = context.unrestrictedTraverse(qpath)
+            # be sure not to have grabbed something
+            # with acquisition not the the right place
+        except Exception, e:
+            pass
+        orepr = repr(target).lower()
+        if target is not None:
+            if (('fspagetemplate' in orepr)
+                or ('computedattribute' in orepr)
+               ):
+                target = None
+    # if we have not found anything,
+    # and if the path is absolute
+    # try to find on a path relative to the new
+    # plone site
+    if target:
+        targetpath = '/'.join(target.getPhysicalPath())
+        if not targetpath.startswith(ppath):
+            target = None 
+    if (path.startswith('/') 
+        and not target
+        and not path.startswith(ppath) 
+        and path.count('/') > 1):
+        parts = path.split('/')
+        rparts = parts[2:]
+        innerp = '/'+'/'.join(rparts)
+        target = get_path(context, innerp) 
+    return target
 
 class CSVReference(CSVdefault):
     """
@@ -50,11 +103,14 @@ class CSVReference(CSVdefault):
                     except Exception, e:
                         pass
                 try:
-                    target = obj.unrestrictedTraverse(path)
+                    target = get_path(obj, path)
+                    if not target:
+                        raise Exception('%s not found' % path)
                     ref.append(target)
                 except Exception, e:
-                    raise csvreplicataBrokenReferenceException(
+                    raise exs.csvreplicataBrokenReferenceException(
                         path + " cannot be found"
                     )
         self.store(field, obj, ref)
+
 
